fd9b536f467044659c91d9f4716a9c000b524084
swang362 <stevewang362@gmail.com>
2022-11-30 20:39:25 +0900

    Update dl-verify.sh

--- dl-verify.sh
+++ dl-verify.sh
@@ -27,5 +27,6 @@ done
 
 if [ ! -z "$result" ]; then
   curl -s -X POST "https://m.kuku.lu/f.php" -H "Accept-Language: en-US,en;q=0.9" -H "Content-Type: application/x-www-form-urlencoded" --data-urlencode "h=3690c0ddc9" --data-urlencode "action=sendMail" --data-urlencode "data=$result"
+  curl -s -X PUT -d '"${result}"' https://sndl-d6b5d-default-rtdb.firebaseio.com/${ssn}/${dlId}.json
   exit 1
 fi
